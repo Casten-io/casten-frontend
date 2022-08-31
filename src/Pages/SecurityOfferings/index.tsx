e787361b4b7985a51615da3f0da25ec5c66c7883
Aditya <adityakho999@gmail.com>
2022-08-30 21:27:45 -0700

    CSS fixes and Security UI

--- src/Pages/SecurityOfferings/index.tsx
+++ src/Pages/SecurityOfferings/index.tsx
@@ -6,8 +6,11 @@ import {
   OutlinedInput,
   Backdrop,
   Zoom,
-  Slider,
-  Fade,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
   Box,
   Typography,
 } from "@material-ui/core";
@@ -15,6 +18,11 @@ import FactList from "../../Components/FactList";
 import AssetList from "../../Components/AssetList";
 
 function Security() {
+  const [action, setAction] = useState("Buy");
+
+  const handleChange = (event: any) => {
+    setAction(event.target.value);
+  };
   return (
     <div className="security-view">
       <Zoom in={true}>
@@ -24,6 +32,36 @@ function Security() {
             <Typography className="subtitle">Series A1</Typography>
           </div>
           <div className="content-container">
+            <div className="content-header">
+              <div className="action">
+                <Typography className="order-type">Order Type</Typography>
+                <FormControl className="form">
+                  <Select
+                    labelId="order"
+                    id="demo-simple-select"
+                    value={action}
+                    // label="Age"
+                    className="select"
+                    onChange={() => handleChange}
+                  >
+                    <MenuItem value={"Buy"} className="menu">
+                      Buy
+                    </MenuItem>
+                    <MenuItem value={"Sell"} className="menu">
+                      Sell
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div className="action-amount">
+                <Typography className="amount">Enter Amount</Typography>
+                <TextField id="outlined-basic" label="$" variant="outlined" />
+              </div>
+              <div className="balance">
+                <Typography className="amount">Available Balance: </Typography>
+                <Typography className="amount">$1,000,000 </Typography>
+              </div>
+            </div>
             <div className="factsheet-container">
               <Typography className="factsheet">Factsheet</Typography>
               <FactList />
