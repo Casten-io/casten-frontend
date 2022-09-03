92b68ccb8e37603dc0b52de35c74e042aa52415b
Aditya <adityakho999@gmail.com>
2022-09-03 16:25:25 -0700

    Works like normal now

--- src/Pages/Portfolio/index.tsx
+++ src/Pages/Portfolio/index.tsx
@@ -5,7 +5,6 @@ import {
   InputAdornment,
   OutlinedInput,
   Backdrop,
-  Zoom,
   Slider,
   Fade,
   Box,
@@ -56,22 +55,18 @@ function Portfolio() {
 
   return (
     <div className="portfolio-view">
-      <Zoom in={true}>
-        <>
-          <div className="header-container">
-            <Typography className="title">Portfolio Manager</Typography>
-          </div>
-          <div className="content-container">
-            <div className="balance-container">
-              <Typography className="balance">Available Balance</Typography>
-              <Typography className="usdc-balance">$ 1,000,000 USDC</Typography>
-            </div>
-            <div className="portfoliolist-container">
-              <PortfolioList />
-            </div>
-          </div>
-        </>
-      </Zoom>
+      <div className="header-container">
+        <Typography className="title">Portfolio Manager</Typography>
+      </div>
+      <div className="content-container">
+        <div className="balance-container">
+          <Typography className="balance">Available Balance</Typography>
+          <Typography className="usdc-balance">$ 1,000,000 USDC</Typography>
+        </div>
+        <div className="portfoliolist-container">
+          <PortfolioList />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Pages/SecurityOfferings/index.tsx
+++ src/Pages/SecurityOfferings/index.tsx
@@ -5,7 +5,6 @@ import {
   InputAdornment,
   OutlinedInput,
   Backdrop,
-  Zoom,
   TextField,
   FormControl,
   InputLabel,
@@ -27,53 +26,49 @@ function Security() {
 
   return (
     <div className="security-view">
-      <Zoom in={true}>
-        <>
-          <div className="header-container">
-            <Typography className="title">Security Details</Typography>
-            <Typography className="subtitle">Series A1</Typography>
+      <div className="header-container">
+        <Typography className="title">Security Details</Typography>
+        <Typography className="subtitle">Series A1</Typography>
+      </div>
+      <div className="content-container">
+        <div className="content-header">
+          <div className="action">
+            <Typography className="order-type">Order Type</Typography>
+            <FormControl className="form">
+              <Select
+                labelId="order"
+                id="demo-simple-select"
+                value={action}
+                className="select"
+                onChange={handleChange}
+              >
+                <MenuItem value={"Buy"} className="menu">
+                  Buy
+                </MenuItem>
+                <MenuItem value={"Sell"} className="menu">
+                  Sell
+                </MenuItem>
+              </Select>
+            </FormControl>
           </div>
-          <div className="content-container">
-            <div className="content-header">
-              <div className="action">
-                <Typography className="order-type">Order Type</Typography>
-                <FormControl className="form">
-                  <Select
-                    labelId="order"
-                    id="demo-simple-select"
-                    value={action}
-                    className="select"
-                    onChange={handleChange}
-                  >
-                    <MenuItem value={"Buy"} className="menu">
-                      Buy
-                    </MenuItem>
-                    <MenuItem value={"Sell"} className="menu">
-                      Sell
-                    </MenuItem>
-                  </Select>
-                </FormControl>
-              </div>
-              <div className="action-amount">
-                <Typography className="amount">Enter Amount</Typography>
-                <TextField id="outlined-basic" label="$" variant="outlined" />
-              </div>
-              <div className="balance">
-                <Typography className="amount">Available Balance: </Typography>
-                <Typography className="amount">$1,000,000 </Typography>
-              </div>
-            </div>
-            <div className="factsheet-container">
-              <Typography className="factsheet">Factsheet</Typography>
-              <FactList />
-            </div>
-            <div className="assetlist-container">
-              <Typography className="assetlist">Asset List</Typography>
-              <AssetList />
-            </div>
+          <div className="action-amount">
+            <Typography className="amount">Enter Amount</Typography>
+            <TextField id="outlined-basic" label="$" variant="outlined" />
           </div>
-        </>
-      </Zoom>
+          <div className="balance">
+            <Typography className="amount">Available Balance: </Typography>
+            <Typography className="amount">$1,000,000 </Typography>
+          </div>
+        </div>
+        <div className="factsheet-container">
+          <Typography className="factsheet">Factsheet</Typography>
+          <FactList />
+        </div>
+        <div className="assetlist-container">
+          <Typography className="assetlist">Asset List</Typography>
+          <AssetList />
+        </div>
+      </div>
     </div>
   );
 }
