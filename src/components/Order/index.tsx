3be64e75a5b52da098a974f9663cd7e3f60ab075
Kunjal Tandel <kunjal.tandel24@gmail.com>
2023-03-22 15:28:32 +0530

    fix: asset details page titles

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -119,98 +119,104 @@ function Order() {
 
   return (
     <div className="main-container">
-      <Card
-        sx={{ minWidth: "300px", maxWidth: "900px" }}
-        className="order-container"
-      >
-        <CardContent className="order-content">
-          <div className="header">
-            <Typography className="status">Status</Typography>
-            <Typography className="status-type">{assetDetails.status}</Typography>
-          </div>
-          <Grid container className="content">
-            <Grid item xs={5} className="item">
-              {/*<div className="unit">*/}
-              {/*  <Typography>Available for financing</Typography>*/}
-              {/*  <Typography>0.00 USDC</Typography>*/}
-              {/*</div>*/}
-              <div className="unit">
-                <Typography>Outstanding</Typography>
-                <Typography>{outstandingAmount} USDC</Typography>
-              </div>
-              <div className="unit">
-                <Typography>Maturity Date</Typography>
-                <Typography>{assetDetails.maturityDate ? new Date(assetDetails.maturityDate * 1000).toDateString() : '-'}</Typography>
-              </div>
-              {/*<div className="unit">*/}
-              {/*  <Typography>Financed By</Typography>*/}
-              {/*  <Typography>0x435838979209..</Typography>*/}
-              {/*</div>*/}
-            </Grid>
-            <Grid item xs={5} className="item">
-              <div className="unit">
-                <Typography>Financing Fee</Typography>
-                <Typography>{(assetDetails.financeFee && `${Number(assetDetails.financeFee || '0').toFixed(2)} %`) || '-'}</Typography>
-              </div>
-              <div className="unit">
-                <Typography>Total Financed</Typography>
-                <Typography>{(assetDetails.principal && `${Number(assetDetails.principal) / (10 ** 6)} USDC`) || '-'}</Typography>
-              </div>
-              <div className="unit">
-                <Typography>Total repaid</Typography>
-                <Typography>{(assetDetails.repaidAmount && `${Number(assetDetails.repaidAmount) / (10 ** 6)} USDC`) || '-'}</Typography>
-              </div>
-              {/*<div className="unit">*/}
-              {/*  <Typography>Financing Fee</Typography>*/}
-              {/*  <Typography>11.33%</Typography>*/}
-              {/*</div>*/}
-            </Grid>
+      <div className="assetlist-container">
+        <Typography className="assetlist">Asset List</Typography>
+        <Card
+          sx={{ minWidth: "300px", maxWidth: "900px" }}
+          className="order-container"
+        >
+          <CardContent className="order-content">
+            <div className="header">
+              <Typography className="status">Status</Typography>
+              <Typography className="status-type">{assetDetails.status}</Typography>
+            </div>
+            <Grid container className="content">
+              <Grid item xs={5} className="item">
+                {/*<div className="unit">*/}
+                {/*  <Typography>Available for financing</Typography>*/}
+                {/*  <Typography>0.00 USDC</Typography>*/}
+                {/*</div>*/}
+                <div className="unit">
+                  <Typography>Outstanding</Typography>
+                  <Typography>{outstandingAmount} USDC</Typography>
+                </div>
+                <div className="unit">
+                  <Typography>Maturity Date</Typography>
+                  <Typography>{assetDetails.maturityDate ? new Date(assetDetails.maturityDate * 1000).toDateString() : '-'}</Typography>
+                </div>
+                {/*<div className="unit">*/}
+                {/*  <Typography>Financed By</Typography>*/}
+                {/*  <Typography>0x435838979209..</Typography>*/}
+                {/*</div>*/}
+              </Grid>
+              <Grid item xs={5} className="item">
+                <div className="unit">
+                  <Typography>Financing Fee</Typography>
+                  <Typography>{(assetDetails.financeFee && `${Number(assetDetails.financeFee || '0').toFixed(2)} %`) || '-'}</Typography>
+                </div>
+                <div className="unit">
+                  <Typography>Total Financed</Typography>
+                  <Typography>{(assetDetails.principal && `${Number(assetDetails.principal) / (10 ** 6)} USDC`) || '-'}</Typography>
+                </div>
+                <div className="unit">
+                  <Typography>Total repaid</Typography>
+                  <Typography>{(assetDetails.repaidAmount && `${Number(assetDetails.repaidAmount) / (10 ** 6)} USDC`) || '-'}</Typography>
+                </div>
+                {/*<div className="unit">*/}
+                {/*  <Typography>Financing Fee</Typography>*/}
+                {/*  <Typography>11.33%</Typography>*/}
+                {/*</div>*/}
+              </Grid>
 
-            {/* <Grid className="item">
-            <Typography>Financing fee</Typography>
-            <Typography>0.00 DAI</Typography>
-          </Grid> */}
-          </Grid>
-        </CardContent>
-      </Card>
-      <TableContainer component={Paper} className="table-container" style={{ marginTop: '20px' }}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table" className="table">
-          <TableHead className="table-head">
-            <TableRow className="head-row">
-              <TableCell className="head-cell">Date</TableCell>
-              <TableCell className="head-cell">Amount</TableCell>
-              <TableCell className="head-cell">Loan</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody className="table-body">
-            {apiCallStatus ? <TableRow className="body-row">
-              <TableCell colSpan={3}>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                  <CircularProgress />
-                </Box>
-              </TableCell>
-            </TableRow> : !repays.length ? <TableRow className="body-row">
-              <TableCell colSpan={3}>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                  <Typography component="span" sx={{ fontSize: 'medium' }}>
-                    No repayments received yet...
-                  </Typography>
-                </Box>
-              </TableCell>
-            </TableRow> : repays.map((row, i) => (
-              <TableRow
-                key={i}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                className="body-row"
-              >
-                <TableCell>{row.blockTimestamp ? new Date(Number(row.blockTimestamp) * 1000).toDateString() : '-'}</TableCell>
-                <TableCell>{(row.currencyAmount && `${Number(row.currencyAmount) / (10 ** 6)} USDC`) || '-'}</TableCell>
-                <TableCell>{row.loan}</TableCell>
+              {/* <Grid className="item">
+              <Typography>Financing fee</Typography>
+              <Typography>0.00 DAI</Typography>
+            </Grid> */}
+            </Grid>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="assetlist-container">
+        <Typography className="assetlist">Asset List</Typography>
+        <TableContainer component={Paper} className="table-container" style={{ marginTop: '20px' }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table" className="table">
+            <TableHead className="table-head">
+              <TableRow className="head-row">
+                <TableCell className="head-cell">Date</TableCell>
+                <TableCell className="head-cell">Amount</TableCell>
+                <TableCell className="head-cell">Loan</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody className="table-body">
+              {apiCallStatus ? <TableRow className="body-row">
+                <TableCell colSpan={3}>
+                  <Box display="flex" justifyContent="center" alignItems="center">
+                    <CircularProgress/>
+                  </Box>
+                </TableCell>
+              </TableRow> : !repays.length ? <TableRow className="body-row">
+                <TableCell colSpan={3}>
+                  <Box display="flex" justifyContent="center" alignItems="center">
+                    <Typography component="span" sx={{ fontSize: 'medium' }}>
+                      No repayments received yet...
+                    </Typography>
+                  </Box>
+                </TableCell>
+              </TableRow> : repays.map((row, i) => (
+                <TableRow
+                  key={i}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  className="body-row"
+                >
+                  <TableCell>{row.blockTimestamp ? new Date(Number(row.blockTimestamp) * 1000).toDateString() : '-'}</TableCell>
+                  <TableCell>{(row.currencyAmount && `${Number(row.currencyAmount) / (10 ** 6)} USDC`) || '-'}</TableCell>
+                  <TableCell>{row.loan}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
       <Card
         sx={{ minWidth: "300px", maxWidth: "900px", marginTop: "30px", display: 'none' }}
         className="order-container"
